e000242099e5222a6403464d2a6ed8662d356de1
alikhan202837 <74448297+alikhan202837@users.noreply.github.com>
2024-01-22 23:16:59 +0600

    lab1

---
+++ lab1/1.py
@@ -0,0 +1,8 @@
+
+#Syntax
+#1
+print("Hello World")
+
+#2
+if 5 > 2:
+    print("YES")
---
+++ lab1/2.py
@@ -0,0 +1,12 @@
+#Comments
+
+
+#1
+#This is comment
+
+#2
+"""
+This is a comment
+written in
+more than just one line
+"""
