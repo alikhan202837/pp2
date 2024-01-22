d3d591db1ebafe488ada627704320987c62f38db
alikhan202837 <74448297+alikhan202837@users.noreply.github.com>
2024-01-22 22:03:46 +0600

    LAB1

---
+++ lab1/1.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+#Syntax
+#1
+print("Hello World")
+#2
+if 5 > 2:
+=======
+#Syntax
+#1
+print("Hello World")
+#2
+if 5 > 2:
+>>>>>>> d93c51f (initial commit)
+    print("YES")
---
+++ lab1/2.py
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+#Comments
+
+
+#1
+#This is comment
+
+#2
+"""
+This is a comment
+written in
+more than just one line
+
+=======
+#Comments
+
+
+#1
+#This is comment
+
+#2
+"""
+This is a comment
+written in
+more than just one line
+
+>>>>>>> d93c51f (initial commit)
+"""
